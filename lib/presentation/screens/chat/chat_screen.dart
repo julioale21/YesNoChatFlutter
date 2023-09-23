9cef737806d812abc8d2150a792b04496cb51b8e
julioale21 <30907777+julioale21@users.noreply.github.com>
2023-09-23 19:59:55 -0300

    first commit

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc5hrlKuG8Ez-TKT5MirOHXeJq2rO5fQ1y8Q&usqp=CAU'),
+          ),
+        ),
+        title: const Text('My Love ❤️'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (context, index) {
+                      return (index % 2 == 0) 
+                        ? const HerMessageBubble()
+                        : const MyMessageBubble();
+                    })),
+            const MessageFieldBox()
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(20),
+    );
+
+    final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "?"',
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: () {
+          final textValue = textController.value.text;
+          print('Value of the field box is: $textValue');
+          textController.clear();
+        },
+      ),
+    );
+
+    return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      decoration: inputDecoration,
+      controller: textController,
+      onFieldSubmitted: (value) {
+        print('submit value: $value');
+        textController.clear();
+        focusNode.requestFocus();
+      },
+    );
+  }
+}
